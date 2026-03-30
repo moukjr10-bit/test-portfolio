8c00a738102f08131bad5eee78ea18155ada6284
moukjr10-bit <moukjr10@gmail.com>
2026-03-30 12:11:10 +0000

    commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
+import { IoMenu } from "react-icons/io5";
 
 function Navbar() {
+  const [openMenu, setOpenMenu] = useState(false);
+
   return (
     <nav className="navbar">
-      {/* LOGO */}
+      
       <h2>Mon Portfolio</h2>
 
-      {/* MENU CENTRE */}
+      {/* MENU NORMAL */}
       <div className="nav-center">
         <NavLink to="/">Accueil</NavLink>
         <NavLink to="/projects">Projets</NavLink>
@@ -15,12 +18,28 @@ function Navbar() {
         <NavLink to="/contact">Contact</NavLink>
       </div>
 
-      {/* LOGIN DROITE */}
+      {/* DROITE */}
       <div className="nav-right">
         <NavLink to="/login" className="login-btn">
           Login
         </NavLink>
+
+        {/* MENU ICON */}
+        <IoMenu 
+          className="menu-icon" 
+          onClick={() => setOpenMenu(!openMenu)} 
+          size={30} 
+        />
       </div>
+
+      {/* MENU MOBILE */}
+      <div className={`mobile-menu ${openMenu ? "active" : ""}`}>
+        <NavLink to="/" onClick={() => setOpenMenu(false)}>Accueil</NavLink>
+        <NavLink to="/projects" onClick={() => setOpenMenu(false)}>Projets</NavLink>
+        <NavLink to="/about" onClick={() => setOpenMenu(false)}>À propos</NavLink>
+        <NavLink to="/contact" onClick={() => setOpenMenu(false)}>Contact</NavLink>
+      </div>
+
     </nav>
   );
 }
